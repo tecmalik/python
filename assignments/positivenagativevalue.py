39727ecef5e1c22e4c707fdb8b52fdd8b666c62e
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-10-24 05:48:13 +0100

    updating assignment

--- assignments/positivenagativevalue.py
+++ assignments/positivenagativevalue.py
@@ -1,18 +1,17 @@
 negativeCounter = 0
-positiveCount = 0
-zeroCount = 0
+positiveCounter = 0
+zeroCounter = 0
 
 for i in range(5):
-    number = float(input("Please enter number {i+1}: "))
+    number = float(input(f"Please enter number {i+1}: "))
 
     if number < 0:
-        negativeCount = negativeCount + 1
+        negativeCounter = negativeCounter + 1
     elif number > 0:
-        positiveCount = positive + 1
+        positiveCounter = positiveCounter + 1
     else:
-        zeroCount = zeroCount + 1
+        zeroCounter = zeroCounter + 1
 
-print(Results:")
-print("Negative numbers: {negativeCount}")
-print("Positive numbers: {positiveCount}")
-print("Zeros: ,zeroCount}")
+print(f"Negative numbers: {negativeCounter}")
+print(f"Positive numbers: {positiveCounter}")
+print(f"Zeros: ,{zeroCounter}")
