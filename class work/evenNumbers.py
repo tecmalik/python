03d5a7a940c52e6887de049bcab72657132fbc31
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-10-25 07:56:24 +0100

    updating assignment

--- class work/evenNumbers.py
+++ class work/evenNumbers.py
@@ -1,4 +1,10 @@
-for count in range (2 , 99 , 2):
-	 print(({count}), end=" ")
-for count in range (2 , 99 , 2):
-	 print((count), end=" \n ")
+initial_point = int(input("Enter initial range: "))
+finish_point = int(input("Enter finishing  range: "))
+given_value = int(input("Enter given value: "))
+
+count_number = 0
+
+for numbers in range (initial_point, finish_point + 1):
+	if numbers % given_value == 0:
+		count_number +=1
+print(count_number)
