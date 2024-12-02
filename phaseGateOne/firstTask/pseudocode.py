2b9c3aa626fff9be906570c3506e4910e0a34531
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-02 15:18:22 +0100

    submission task 3

---
+++ phaseGateOne/firstTask/pseudocode.py
@@ -0,0 +1,21 @@
+pseudocode 
+prompt the user to enter first number 
+collect number
+store as firstinteger 
+prompt the user to enter second number 
+collect number
+store as secondinteger
+prompt the user to enter third number 
+collect number
+store as thirdinteger
+if first number greater then second number store ad largest 
+if firstNumber<secondNumbe><thirdNumber:
+	print(f"{firstnumber},(secondNumber},{thirdnumber}")
+else if secondNumbe<firstNumber<thirdNumber:
+	print(f"(secondNumber},{firstnumber},{thirdnumber}")
+else if thirdNumber<secondNumbe<firstNumber:
+	print(f"{thirdnumber},{firstnumber},{secondNumber}")
+else if secondNumbe<thirdNumber<firstNumber:
+	print(f"{secondNumber},{thirdnumber},{firstnumber}")
+else if firstNumber<thirdNumber<secondNumbe:
+	print(f"{secondNumber},{thirdnumber},{firstnumber}")
---
+++ phaseGateOne/firstTask/task3.py
@@ -0,0 +1,13 @@
+firstNumber = int(input(enter first Number :))
+secondNumber = int(input(enter second Number :))
+thirdNumber = int(input(enter third Number :))
+ if firstNumber<secondNumbe><thirdNumber:
+	print(f"{firstnumber},(secondNumber},{thirdnumber}")
+elif if secondNumbe<firstNumber<thirdNumber:
+	print(f"(secondNumber},{firstnumber},{thirdnumber}")
+elif if thirdNumber<secondNumbe<firstNumber:
+	print(f"{thirdnumber},{firstnumber},{secondNumber}")
+elif if secondNumbe<thirdNumber<firstNumber:
+	print(f"{secondNumber},{thirdnumber},{firstnumber}")
+elif if firstNumber<thirdNumber<secondNumbe:
+	print(f"{secondNumber},{thirdnumber},{firstnumber}")
