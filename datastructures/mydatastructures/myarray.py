b1dc8c7e92f59f44bd6050416834ea05ac073316
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2025-01-25 12:05:26 +0100

    updating files

---
+++ datastructures/mydatastructures/myarray.py
@@ -0,0 +1,8 @@
+class MyArray:
+    def __init__(self):
+        self.array = []
+        self.size = 0
+
+    def __self__ (self):
+        return {f"{MyArrayList}"}
+
---
+++ datastructures/testmydatastructure/myarray.py
@@ -0,0 +1,6 @@
+import unittest
+from mydatastructures import myarray
+
+class MyArray(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False) 
